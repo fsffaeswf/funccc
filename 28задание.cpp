fa12fc997b3d9a3c69dd51fbaa71ad82d32235b0
fsffaeswf <114169974+fsffaeswf@users.noreply.github.com>
2022-11-22 19:05:13 +0300

    Add files via upload

---
+++ 28задание.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "isprime.h"
+#include <ctime>
+using namespace std;
+int main() {
+	const int N = 10;
+	int arr[N], k = 0;
+	srand(time(nullptr));
+	for (int i = 0; i < 10; i++) {
+		arr[i] = rand() % 26;
+		cout << arr[i] << ' ';
+	}
+		for (int i = 0; i < 10; i++) {
+			if (isprime(arr[i]) == 1) {
+				k++;
+			}
+		}
+	cout << endl << k;
+}
